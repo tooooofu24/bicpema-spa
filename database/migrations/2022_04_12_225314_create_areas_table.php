10f3c3f1a4d8d7c403e8b87f7a35d50bbdb8690b
toya <toya24xxx@i.softbank.jp>
2022-04-13 08:14:12 +0900

    Areaテーブルの作成

---
+++ database/migrations/2022_04_12_225314_create_areas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('target', ['小学生', '中学生', '高校生']);
+            $table->enum('subject', ['物理', '科学', '生物', '地学', '数学']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+}
